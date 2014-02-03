7c60764a3ec90a7231f90eb164ad41fb38c76eb4
MartinezAvellan <martinez.avellan@email.com>
2014-02-03 14:14:28 -0200

    WS  EJB JBOSS MAVEN

---
+++ Teste/src/br/com/brq/ws/TesteWS.java
@@ -0,0 +1,17 @@
+package br.com.brq.ws;
+
+import javax.ejb.Stateless;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+
+@WebService
+@Stateless
+public class TesteWS {
+	
+	@WebMethod
+	public int calculaValores(@WebParam(name="valorUm") int valorUm, @WebParam(name="valorDois") int valorDois){
+		return valorUm + valorDois;
+	}
+
+}
